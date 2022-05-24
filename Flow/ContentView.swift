456c36abcc6201d0bb8339fd0bf0a16cd0ce6527
ChaseChoi <caiguihao@gmail.com>
2022-05-24 23:04:18 +0800

    Initial Commit

---
+++ Flow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flow
+//
+//  Created by Chase Choi on 2022/5/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Flow/FlowApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlowApp.swift
+//  Flow
+//
+//  Created by Chase Choi on 2022/5/24.
+//
+
+import SwiftUI
+
+@main
+struct FlowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
